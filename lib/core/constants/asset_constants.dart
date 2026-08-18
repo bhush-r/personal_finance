0792674dbf070e2eaeed12576ed5be167f8afd38
Bhushan Lilhare <bhushanlilhare956@gmail.com>
2026-08-19 02:04:41 +0530

    Add Firebase and project configuration

---
+++ lib/core/constants/asset_constants.dart
@@ -0,0 +1,15 @@
+class AssetConstants {
+  // Base paths
+  static const String _imagePath = 'assets/images';
+  static const String _iconPath = 'assets/icons';
+  static const String _illustrationPath = 'assets/illustrations';
+
+  // Illustrations
+  static const String emptyBox = '$_illustrationPath/empty_box.png';
+  static const String wallet = '$_illustrationPath/wallet.png';
+  static const String error404 = '$_illustrationPath/error_404.png';
+  static const String successCheck = '$_illustrationPath/success_check.png';
+
+  // Icons (If using custom SVGs or PNGs)
+  static const String appLogo = '$_imagePath/logo.png';
+}
